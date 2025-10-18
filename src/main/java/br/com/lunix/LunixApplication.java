fd2435ef1ad49fac9da1ac871f663d9a1b97c580
SunaUezuri <wesley.jane.santos@gmail.com>
2025-10-18 20:09:34 -0300

    Criando a base do projeto para inicialização

---
+++ src/main/java/br/com/lunix/LunixApplication.java
@@ -0,0 +1,13 @@
+package br.com.lunix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunixApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/lunix/config/Template.java
@@ -0,0 +1,4 @@
+package br.com.lunix.config;
+
+public class Template {
+}
