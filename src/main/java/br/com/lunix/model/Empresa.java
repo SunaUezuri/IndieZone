87619119eb09b005ba64c7bb995814a3c4bd6fd1
SunaUezuri <wesley.jane.santos@gmail.com>
2025-10-18 20:30:13 -0300

    Modelando a classe empresa para informações sobre a produtora dos jogos

--- src/main/java/br/com/lunix/model/Empresa.java
+++ src/main/java/br/com/lunix/model/Empresa.java
@@ -1,4 +1,26 @@
 package br.com.lunix.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "empresa")
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Empresa {
+
+    @Id
+    private String id;
+
+    private String nome;
+
+    private String descricao;
+
+    private String paisOrigem;
+
+    private String urlLogo;
 }
