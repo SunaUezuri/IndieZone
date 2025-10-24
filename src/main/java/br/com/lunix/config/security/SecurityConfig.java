71dbf3dc0b91b066c57dbfc61c9276216b990784
SunaUezuri <wesley.jane.santos@gmail.com>
2025-10-23 22:31:44 -0300

    feat: classes de configuração do Security criadas

---
+++ src/main/java/br/com/lunix/config/security/SecurityConfig.java
@@ -0,0 +1,55 @@
+package br.com.lunix.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authz -> authz
+                        // Rotas públicas
+                        .requestMatchers("/css/**", "/js/**", "/images/**", "/webjars/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/", "/jogos", "/jogos/**", "/empresas", "/empresas/**", "/sobre").permitAll()
+                        .requestMatchers("/login", "/registro/**").permitAll()
+
+                        // Permissões de Dev
+                        .requestMatchers("/dev/**").hasRole("DEV")
+
+                        // Permissões de ADM
+                        .requestMatchers("/admin").hasRole("ADMIN")
+
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .loginProcessingUrl("/login")
+                        .defaultSuccessUrl("/", true)
+                        .failureUrl("/login?error")
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login?logout")
+                        .permitAll()
+                );
+
+                return http.build();
+    }
+}
--- src/main/java/br/com/lunix/model/enums/Role.java
+++ src/main/java/br/com/lunix/model/enums/Role.java
@@ -2,6 +2,6 @@ package br.com.lunix.model.enums;
 
 public enum Role {
     ROLE_USER,
-    ROLE_ADM,
+    ROLE_ADMIN,
     ROLE_DEV
 }
