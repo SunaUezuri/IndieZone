c9ef2318d3fb30d35072b1164d04fab6b303eac2
SunaUezuri <wesley.jane.santos@gmail.com>
2025-10-30 22:04:00 -0300

    doc: adicionando comentários para JogoMapeadoDto e a migration 005

--- src/main/java/br/com/lunix/config/migrations/V1_005__CreatePlatformIndex.java
+++ src/main/java/br/com/lunix/config/migrations/V1_005__CreatePlatformIndex.java
@@ -7,9 +7,19 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.index.Index;
 
+/*
+    Classe de migração responsável por criar um
+    indice para o campo de plataformas criando
+    pesquisas mais dinâmicas.
+*/
 @ChangeUnit(id = "create-platform-index", order = "005", author = "lunix-dev")
 public class V1_005__CreatePlatformIndex {
 
+    /*
+        Método de execução que cria o indice
+        no campo plataformas da collection de
+        jogos.
+    */
     @Execution
     public void createPlatformIndex(MongoTemplate template) {
         System.out.println("MONGOCK[005]: Criando um índice multichave em jogos.plataformas");
@@ -17,6 +27,10 @@ public class V1_005__CreatePlatformIndex {
                 .createIndex(new Index().on("plataformas", Sort.Direction.ASC));
     }
 
+    /*
+        ROllback em caso de problemas deletando
+        o indice criado.
+    */
     @RollbackExecution
     public void rollback(MongoTemplate template) {
         template.indexOps("jogos").dropIndex("plataformas_1");
--- src/main/java/br/com/lunix/dto/jogos/JogoMapeadoDto.java
+++ src/main/java/br/com/lunix/dto/jogos/JogoMapeadoDto.java
@@ -2,6 +2,10 @@ package br.com.lunix.dto.jogos;
 
 import br.com.lunix.model.entities.Jogo;
 
+/*
+    DTO responsável por receber o objeto mapeado
+    pelo RawgMapper.
+*/
 public record JogoMapeadoDto(
         Jogo jogo,
         String nomeDesenvolvedorPrincipal
